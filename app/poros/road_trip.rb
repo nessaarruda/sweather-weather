69c9d355ac7fe8969fa77d94d042e052e7177639
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-02 15:57:13 -0700

    update  poro for road trip weather info

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,9 +1,13 @@
 class RoadTrip
-  attr_reader :start_city, :end_city, :travel_time
+  attr_reader :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
 
-  def initialize(data, start_city, end_city)
+  def initialize(data, start_city, end_city, forecast)
     @start_city = start_city
     @end_city = end_city
     @travel_time = data[:route][:formattedTime]
+    @weather_at_eta = forecast
   end
 end
