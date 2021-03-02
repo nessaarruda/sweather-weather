3b20439e242738487220a5d8f8720e6163d9089e
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-02 10:04:30 -0700

    Add route for background

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace :v1 do
-      get '/backgrounds', to: 'background#index'
+      get '/backgrounds', to: 'backgrounds#index'
     end
   end
 end
