4c0c6a045c7e3a24665e5d038ad7d881afb5aba6
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-02 10:42:55 -0700

    Add sessio method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
