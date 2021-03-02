b981c224001251783a3a19fdd9f35eaa5d12ff9c
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-02 10:14:13 -0700

    Add routes for users create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace :v1 do
+      resources :users, only: :create
       get '/backgrounds', to: 'backgrounds#index'
     end
   end
