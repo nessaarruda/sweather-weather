414ad43edc9bbdeb848af15cbd80db41da5fc2fd
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-02 10:14:28 -0700

    Add users controller and create action

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    render json: UserSerializer.new(user), status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
