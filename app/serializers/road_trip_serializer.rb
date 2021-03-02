03441e4c1c3e74287d88a2b859639b1930de858b
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-02 12:06:12 -0700

    Add road trip serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -2,9 +2,8 @@ class RoadTripSerializer
   include FastJsonapi::ObjectSerializer
   set_id { nil }
   set_type :roadtrip
-  
+
   attributes :start_city,
              :end_city,
-             :travel_time,
-             :weather_at_eta
+             :travel_time
 end
