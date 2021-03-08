621c41618d0499a42d3dc803c6273b292e91015e
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-08 10:18:40 -0700

    start converting unix time to determine time of arrival

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -1,7 +1,14 @@
 class YelpFacade
   class << self
-    def food(start, destination, food_type)
-      YelpService.get_food_data(food_type, destination)
+    def food(start, destination, food)
+      trip_data = RoadTripFacade.get_data(start, destination)
+      arrival_time = unix_time_converter(trip_data.travel_time)
+      food_data = YelpService.get_food_data(food, destination, arrival_time)
+      Food.new(food_data, trip_data)
+    end
+
+    def unix_time_converter(travel_time)
+
     end
   end
 end
