2ad1ab529f586f95ac0db6ed8f46972431c7bce0
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-08 11:40:03 -0700

    add conditional to cover sad path

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::MunchiesController < ApplicationController
 
   def index
-    food = YelpFacade.food(params[:start], params[:destination], params[:food])
-
-    render json: YelpSerializer.new(food), status: 200
+    if !params[:food].empty?
+      food = YelpFacade.food(params[:start], params[:destination], params[:food])
+      render json: YelpSerializer.new(food), status: 200
+    else
+      render json: 'Please provide type of food' , status: 401
+    end
   end
 end
