977dc8b05647869c74004e834c5d3228a10707f0
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-08 16:46:50 -0700

    refactor facade tests

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 
-describe ForecastFacade do
-  describe 'happy path' do
-    it 'returns the forecast for a city' do
-      VCR.use_cassette('denver_forecast') do
-        forecast = ForecastFacade.forecast('Denver, Co')
+describe 'Forecast Facade' do
+  it 'returns the forecast for a city' do
+    VCR.use_cassette('denver_forecast') do
+      forecast = ForecastFacade.forecast('Denver, CO')
 
-        expect(forecast).to be_a(Forecast)
-        expect(forecast.current_forecast).to be_a(CurrentForecast)
-        expect(forecast.daily_forecast).to be_an(Array)
+      expect(forecast).to be_a(Forecast)
+      expect(forecast.current_forecast).to be_a(CurrentForecast)
+      expect(forecast.daily_forecast).to be_an(Array)
 
-        forecast.daily_forecast.each do |daily|
-          expect(daily).to be_a(DailyForecast)
-        end
+      forecast.daily_forecast.each do |daily|
+        expect(daily).to be_a(DailyForecast)
+      end
 
-        expect(forecast.hourly_forecast).to be_an(Array)
+      expect(forecast.hourly_forecast).to be_an(Array)
 
-        forecast.hourly_forecast.each do |hourly|
-          expect(hourly).to be_a(HourlyForecast)
-        end
+      forecast.hourly_forecast.each do |hourly|
+        expect(hourly).to be_a(HourlyForecast)
       end
     end
   end
 end
-    
--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
 describe 'Road Trip Facade' do
-  describe 'happy path' do
-    it 'returns road trip poro based on start_city and end_city' do
-      VCR.use_cassette('route_ny_la') do
+  it 'returns road trip poro based on start_city and end_city' do
+    VCR.use_cassette('route_ny_la') do
+      details = RoadTripFacade.get_data('New York, NY', 'Los Angeles, CA')
 
-        # user = create(:user) #not using this, I think I need to
-
-        details = RoadTripFacade.get_data('New York, NY', 'Los Angeles, CA')
-
-        expect(details).to be_a(RoadTrip)
-        expect(details.start_city).to eq('New York, NY')
-        expect(details.end_city).to eq('Los Angeles, CA')
-        expect(details.travel_time).to be_a(String)
-        expect(details.weather_at_eta).to be_a(Hash)
-        expect(details.weather_at_eta).to have_key(:temperature)
-        expect(details.weather_at_eta[:temperature]).to be_a(Float)
-        expect(details.weather_at_eta).to have_key(:conditions)
-        expect(details.weather_at_eta[:conditions]).to be_a(String)
-      end
+      expect(details).to be_a(RoadTrip)
+      expect(details.start_city).to eq('New York, NY')
+      expect(details.end_city).to eq('Los Angeles, CA')
+      expect(details.travel_time).to be_a(String)
+      expect(details.weather_at_eta).to be_a(Hash)
+      expect(details.weather_at_eta).to have_key(:temperature)
+      expect(details.weather_at_eta[:temperature]).to be_a(Float)
+      expect(details.weather_at_eta).to have_key(:conditions)
+      expect(details.weather_at_eta[:conditions]).to be_a(String)
     end
   end
 end
